b29045f8d54337a1434eb3a3785039b26a11ea43
Diyorbek4926 <131335654+Diyorbek4926@users.noreply.github.com>
2024-08-17 20:50:01 +0500

    Commit 8

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,12 +1,11 @@
 sealed class Constants {
-  static const String botToken = "6278652695:AAE4BqnCejJafp4QTFLwx_361RmM4HTgZKE";
-  // static const String botToken = "7429063114:AAEmIo9orDdnA8Vt3ZXYVuB72-MBUb_cY0M";
+  static const String botToken = "7429063114:AAEmIo9orDdnA8Vt3ZXYVuB72-MBUb_cY0M";
   static const String cardNumber = "5614 6812 1614 1503";
   static const String cardNumberHolder = "Kamoljon Hamzayev";
   static const int subscriptionPrice = 20000;
   static const int subscriptionPeriod = 72;
-  static const int adminId = 2119398453;
-  // static const int adminId = 1480826067;
+  // static const int adminId = 2119398453;
+  static const int adminId = 1480826067;
 
   /// database
   static const String host = "localhost";
